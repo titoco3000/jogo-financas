827c46d2544e5d25d4bb32a0289d8a1d2fc33740
Guilherme Chiarelli <guigui.dichiare@gmail.com>
2025-05-22 04:38:22 -0300

    guichiare

---
+++ src/scenes/menu_intro.py
@@ -0,0 +1,46 @@
+import pygame
+from src.classes.botao import Botao
+from src.classes.text import Text
+from src.classes.gameobject import GameObject
+import src.globals as globals
+
+
+def run(screen):
+    running = True
+    
+    def iniciar_jogo():
+        nonlocal running
+        running = False
+
+    background = pygame.image.load("assets/sprites/menu.png")
+    text = Text((100,100), "intro")
+
+    GameObject.clear_scene()
+    Botao(
+        pygame.rect.Rect(330, 500, 300, 50),
+        "Começar",
+        border_color=(100, 100, 100),
+        border_width=2,
+        radius=10,
+        on_click=iniciar_jogo,
+    )
+
+    while running:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                globals.sair = True
+                return
+
+        GameObject.update_all(events)
+
+        screen.fill((0, 0, 0))
+        screen.blit(background, (0, 0))
+        GameObject.draw_all(screen)
+
+        pygame.display.flip()
+
+    globals.volume = 1
+    # outras configs
+
+    return
